bd3042feaa15a6834df30bbfa2527a53071515e8
hender-hs <henderson.f.p@hotmail.com>
2022-06-08 20:03:26 -0300

    refact: refact the code and improve it

--- index.ts
+++ index.ts
@@ -1,15 +1,13 @@
-import WebSocket, { WebSocketServer } from 'ws';
+import WebSocket, { WebSocketServer } from 'ws'
+import WebsocketController from './src/controller/index.controller'
+import 'dotenv/config'
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function message(data, isBinary) {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data, { binary: isBinary });
-      }
-    });
-  });
-});
+const initiateWSS = () => {
+  	const websocketManager = new WebsocketController(wss, WebSocket)
+  	websocketManager.init()
+}
+initiateWSS()
 
-console.log("running on port %i", 8080)
+console.log("running on port ", 8080)
